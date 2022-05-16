6d8516c7d35154f76a858213fd32660de9846796
Amanda Rafaela Barreto de Melo <arafaela@uolinc.com>
2022-05-16 18:30:58 -0300

    merge

--- src/pages/RegisterUser/index.js
+++ src/pages/RegisterUser/index.js
@@ -3,28 +3,28 @@ import { useNavigate, useLocation } from "react-router-dom";
 import InputMask from "react-input-mask";
 import { toast } from "react-toastify";
 import S3 from "react-aws-s3";
+import ResponsiveAppBar from "../../components/bar";
+import Select from 'react-select'
+
+import { Form, Row, Col, Button } from 'react-bootstrap';
 
 import {
   MainContainer,
-  InputTitle,
-  NormalText,
+  ContentRegister,
   Title,
-  ContainerInputs,
-  ButtonContainer,
-  ButtonContainerText,
-  ContainerModal,
-  TitleModal,
+  ContentForm,
+  ContentButton
 } from "./styles";
 
 function RegisterUser() {
   const [path, setPath] = useState("");
-  const [openModal, setOpenModal] = useState(false);
   const [name, setName] = useState("");
   const [cpf, setCPF] = useState("");
   const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [uf, setUF] = useState("");
-  const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [crfa, setCrfa] = useState("");
+  const [cep, setCep] = useState("");
+  const [specialty, setSpecialty] = useState("");
 
   const navigate = useNavigate();
   let params = useLocation();
@@ -46,12 +46,12 @@ function RegisterUser() {
     try {
       const toSend = {
         email,
-        password,
         name,
-        userType: path === "profissional" ? "Doutor" : "Paciente",
-        phone,
         cpf,
-        state: uf,
+        address,
+        crfa,
+        cep,
+        specialty
       };
 
       const resp = await fetch("http://18.212.199.176:3001/users/", {
@@ -63,7 +63,7 @@ function RegisterUser() {
         body: JSON.stringify(toSend),
       });
       const res = await resp.json();
-      console.log(res.message);
+
       if (res.message === "User created") {
         navigate("/login", {
           state: {
@@ -78,119 +78,90 @@ function RegisterUser() {
     }
   };
 
+
   return (
-    <MainContainer style={{ background: openModal ? "#C9C9C9" : "#FFF" }}>
-      <Title>Faça seu cadastro</Title>
-      <NormalText>Preencha as informações abaixo</NormalText>
-      <ContainerInputs>
-        <InputTitle>Nome</InputTitle>
-        <InputMask
-          onChange={(e) => setName(e.target.value)}
-          placeholder="João Pires Alves"
-          style={{
-            width: "400px",
-            border: "1px solid #000",
-            borderRadius: "10px",
-            height: "40px",
-            paddingLeft: "15px",
-            background: "none",
-            marginTop: "15px",
-            marginBottom: "20px",
-          }}
-        />
-        <InputTitle>CPF</InputTitle>
-        <InputMask
-          onChange={(e) => setCPF(e.target.value)}
-          placeholder="000.000.000-00"
-          mask="999.999.999-99"
-          style={{
-            width: "400px",
-            border: "1px solid #000",
-            borderRadius: "10px",
-            height: "40px",
-            paddingLeft: "15px",
-            background: "none",
-            marginTop: "15px",
-            marginBottom: "20px",
-          }}
-        />
-        <InputTitle>E-mail</InputTitle>
-        <InputMask
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="email@com"
-          style={{
-            width: "400px",
-            border: "1px solid #000",
-            borderRadius: "10px",
-            height: "40px",
-            paddingLeft: "15px",
-            background: "none",
-            marginTop: "15px",
-            marginBottom: "20px",
-          }}
-        />
-        <InputTitle>Telefone</InputTitle>
-        <InputMask
-          onChange={(e) => setPhone(e.target.value)}
-          placeholder="(11) 11111-1111"
-          mask="(99) 99999-9999"
-          style={{
-            width: "400px",
-            border: "1px solid #000",
-            borderRadius: "10px",
-            height: "40px",
-            paddingLeft: "15px",
-            background: "none",
-            marginTop: "15px",
-            marginBottom: "20px",
-          }}
-        />
-        {path === "profissional" ? (
-          <>
-            <InputTitle>UF</InputTitle>
-            <InputMask
-              onChange={(e) => setUF(e.target.value)}
-              placeholder="SP"
-              style={{
-                width: "400px",
-                border: "1px solid #000",
-                borderRadius: "10px",
-                height: "40px",
-                paddingLeft: "15px",
-                background: "none",
-                marginTop: "15px",
-                marginBottom: "20px",
-              }}
-            />
-          </>
-        ) : (
-          <></>
-        )}
-        <InputTitle>Senha</InputTitle>
-        <InputMask
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Senha"
-          type="password"
-          style={{
-            width: "400px",
-            border: "1px solid #000",
-            borderRadius: "10px",
-            height: "40px",
-            paddingLeft: "15px",
-            background: "none",
-            marginTop: "15px",
-            marginBottom: "20px",
-          }}
-        />
-        <ButtonContainer onClick={() => register()}>
-          <ButtonContainerText>Cadastrar</ButtonContainerText>
-        </ButtonContainer>
-      </ContainerInputs>
-      {openModal && (
-        <ContainerModal>
-          <TitleModal>Faça o upload dos seus documentos</TitleModal>
-        </ContainerModal>
-      )}
+    <MainContainer style={{ background: "#FFF" }}>
+      <ResponsiveAppBar />
+      <ContentRegister>
+        <Title>Faça seu cadastro</Title>
+        <ContentForm>
+          <Form>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridName">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Nome completo</Form.Label>
+                <Form.Control placeholder="Ex.: João da Silva" onChange={(e) => setName(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>E-mail</Form.Label>
+                <Form.Control type="email" placeholder="Ex.: email@email.com" onChange={(e) => setEmail(e.target.value)} />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCpf">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>CPF</Form.Label>
+                <Form.Control placeholder="Ex.: 000.000.000-00" onChange={(e) => setCPF(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridAddress">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Endereço completo</Form.Label>
+                <Form.Control
+                  placeholder="Ex.: Avenida Brasil, 23 - São Paulo - SP"
+                  onChange={(e) => setAddress(e.target.value)} />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCrfa">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Anexar CRFA ou Comprovante da faculdade</Form.Label>
+                <Form.Control type="file" placeholder=".pdf, .jpeg, .png" onChange={(e) => setCrfa(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridCep">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>CEP</Form.Label>
+                <Form.Control placeholder="Ex.: 00000-000" onChange={(e) => setCep(e.target.value)} />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Especialidade</Form.Label>
+                <Form.Select defaultValue="Especialidade">
+                  <option value="Especialidade"> Especialidade </option>
+                  <option value="Audiologia"> Audiologia </option>
+                  <option value="Linguagem"> Linguagem </option>
+                  <option value="Motricidade Orofacial"> Motricidade Orofacial </option>
+                  <option value="Saúde Coletiva"> Saúde Coletiva </option>
+                  <option value="Voz"> Voz </option>
+                  <option value="Disfagia"> Disfagia </option>
+                  <option value="Fonoaudiologia Educacional"> Fonoaudiologia Educacional </option>
+                  <option value="Gerontologia"> Gerontologia </option>
+                  <option value="Fonoaudiologia Neurofuncional"> Fonoaudiologia Neurofuncional </option>
+                  <option value="Fonoaudiologia do Trabalho"> Fonoaudiologia do Trabalho </option>
+                  <option value="Neuropsicologia"> Neuropsicologia </option>
+                  <option value="Fluência"> Fluência </option>
+                  <option value="Perícia Fonoaudiológica"> Perícia Fonoaudiológica </option>
+                  <option value="Fonoaudiologia Hospitalar"> Fonoaudiologia Hospitalar </option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <ContentButton>
+                  <Button
+                    style={{
+                      width: "300px",
+                      borderRadius: "30px",
+                      backgroundColor: "#1E98D4",
+                      color: "#fff",
+                      border: "none",
+                      cursor: "pointer"
+                    }}>
+                    Avançar Cadastro
+                  </Button>
+                </ContentButton>
+              </Form.Group>
+            </Row>
+          </Form>
+        </ContentForm>
+      </ContentRegister>
     </MainContainer>
   );
 }

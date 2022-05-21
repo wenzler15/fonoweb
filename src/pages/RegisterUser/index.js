e5201c460d04ce858926838c66465e90d371a901
guilhermeEloi <gui.eloi.silva@gmail.com>
2022-05-21 17:36:50 -0300

    ajuste de campo para senha

--- src/pages/RegisterUser/index.js
+++ src/pages/RegisterUser/index.js
@@ -4,16 +4,16 @@ import InputMask from "react-input-mask";
 import { toast } from "react-toastify";
 import S3 from "react-aws-s3";
 import ResponsiveAppBar from "../../components/bar";
-import Select from 'react-select'
+import Select from "react-select";
 
-import { Form, Row, Col, Button, Modal } from 'react-bootstrap';
+import { Form, Row, Col, Button, Modal } from "react-bootstrap";
 
 import {
   MainContainer,
   ContentRegister,
   Title,
   ContentForm,
-  ContentButton
+  ContentButton,
 } from "./styles";
 
 function RegisterUser() {
@@ -24,6 +24,7 @@ function RegisterUser() {
   const [address, setAddress] = useState("");
   const [crfa, setCrfa] = useState("");
   const [cep, setCep] = useState("");
+  const [password, setPassword] = useState("");
   const [specialty, setSpecialty] = useState("");
   const [show, setShow] = useState(false);
   const [urlPayment, setUrl] = useState("");
@@ -51,36 +52,39 @@ function RegisterUser() {
       const toSend = {
         email,
         name,
+        password,
         cpf,
         address,
         crfa,
         cep,
-        specialty
+        specialty,
       };
 
       const preference = {
         items: [
           {
-            title: 'Assinatura',
+            title: "Assinatura",
             quantity: 1,
-            unit_price: 129
-          }
-        ]
+            unit_price: 129,
+          },
+        ],
       };
 
       //navigate('/payment', { state: toSend });
-      const resp = await fetch("https://api.mercadopago.com/checkout/preferences?access_token=TEST-839873972326160-051700-d50edb93763bcf54c251226707e01692-146149588", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(preference),
-      });
+      const resp = await fetch(
+        "https://api.mercadopago.com/checkout/preferences?access_token=TEST-839873972326160-051700-d50edb93763bcf54c251226707e01692-146149588",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(preference),
+        }
+      );
       const res = await resp.json();
       setUrl(res.sandbox_init_point);
-      setShow(true)
-
+      setShow(true);
     } catch (err) {
       console.log(err);
     }
@@ -120,7 +124,6 @@ function RegisterUser() {
     }*/
   };
 
-
   return (
     <MainContainer style={{ background: "#E5E5E5" }}>
       <ContentRegister>
@@ -129,73 +132,142 @@ function RegisterUser() {
           <Form>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridName">
-                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Nome completo</Form.Label>
-                <Form.Control placeholder="Ex.: João da Silva" onChange={(e) => setName(e.target.value)} />
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  Nome completo
+                </Form.Label>
+                <Form.Control
+                  placeholder="Ex.: João da Silva"
+                  onChange={(e) => setName(e.target.value)}
+                />
                 <Form.Control hidden value={urlPayment} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>E-mail</Form.Label>
-                <Form.Control type="email" placeholder="Ex.: email@email.com" onChange={(e) => setEmail(e.target.value)} />
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  E-mail
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Ex.: email@email.com"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </Form.Group>
             </Row>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridCpf">
-                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>CPF</Form.Label>
-                <Form.Control placeholder="Ex.: 000.000.000-00" as={InputMask} mask='999.999.999-99' onChange={(e) => setCPF(e.target.value)} />
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  CPF
+                </Form.Label>
+                <Form.Control
+                  placeholder="Ex.: 000.000.000-00"
+                  as={InputMask}
+                  mask="999.999.999-99"
+                  onChange={(e) => setCPF(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridAddress">
-                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Endereço completo</Form.Label>
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  Endereço completo
+                </Form.Label>
                 <Form.Control
                   placeholder="Ex.: Avenida Brasil, 23 - São Paulo - SP"
-                  onChange={(e) => setAddress(e.target.value)} />
+                  onChange={(e) => setAddress(e.target.value)}
+                />
               </Form.Group>
             </Row>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridCrfa">
-                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Anexar CRFA ou Comprovante da faculdade</Form.Label>
-                <Form.Control type="file" placeholder=".pdf, .jpeg, .png" onChange={(e) => setCrfa(e.target.value)} />
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  Anexar CRFA ou Comprovante da faculdade
+                </Form.Label>
+                <Form.Control
+                  type="file"
+                  placeholder=".pdf, .jpeg, .png"
+                  onChange={(e) => setCrfa(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridCep">
-                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>CEP</Form.Label>
-                <Form.Control placeholder="Ex.: 00000-000" as={InputMask} mask='99999-999' onChange={(e) => setCep(e.target.value)} />
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  CEP
+                </Form.Label>
+                <Form.Control
+                  placeholder="Ex.: 00000-000"
+                  as={InputMask}
+                  mask="99999-999"
+                  onChange={(e) => setCep(e.target.value)}
+                />
               </Form.Group>
             </Row>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridEspecialidade">
-                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>Especialidade</Form.Label>
-                <Form.Select defaultValue="Especialidade" onChange={(e) => setSpecialty(e.target.value)}>
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  Especialidade
+                </Form.Label>
+                <Form.Select
+                  defaultValue="Especialidade"
+                  onChange={(e) => setSpecialty(e.target.value)}
+                >
                   <option value="Especialidade"> Especialidade </option>
                   <option value="Audiologia"> Audiologia </option>
                   <option value="Linguagem"> Linguagem </option>
-                  <option value="Motricidade Orofacial"> Motricidade Orofacial </option>
+                  <option value="Motricidade Orofacial">
+                    {" "}
+                    Motricidade Orofacial{" "}
+                  </option>
                   <option value="Saúde Coletiva"> Saúde Coletiva </option>
                   <option value="Voz"> Voz </option>
                   <option value="Disfagia"> Disfagia </option>
-                  <option value="Fonoaudiologia Educacional"> Fonoaudiologia Educacional </option>
+                  <option value="Fonoaudiologia Educacional">
+                    {" "}
+                    Fonoaudiologia Educacional{" "}
+                  </option>
                   <option value="Gerontologia"> Gerontologia </option>
-                  <option value="Fonoaudiologia Neurofuncional"> Fonoaudiologia Neurofuncional </option>
-                  <option value="Fonoaudiologia do Trabalho"> Fonoaudiologia do Trabalho </option>
+                  <option value="Fonoaudiologia Neurofuncional">
+                    {" "}
+                    Fonoaudiologia Neurofuncional{" "}
+                  </option>
+                  <option value="Fonoaudiologia do Trabalho">
+                    {" "}
+                    Fonoaudiologia do Trabalho{" "}
+                  </option>
                   <option value="Neuropsicologia"> Neuropsicologia </option>
                   <option value="Fluência"> Fluência </option>
-                  <option value="Perícia Fonoaudiológica"> Perícia Fonoaudiológica </option>
-                  <option value="Fonoaudiologia Hospitalar"> Fonoaudiologia Hospitalar </option>
+                  <option value="Perícia Fonoaudiológica">
+                    {" "}
+                    Perícia Fonoaudiológica{" "}
+                  </option>
+                  <option value="Fonoaudiologia Hospitalar">
+                    {" "}
+                    Fonoaudiologia Hospitalar{" "}
+                  </option>
                 </Form.Select>
               </Form.Group>
-
+              <Form.Group as={Col} controlId="formGridCep">
+                <Form.Label style={{ fontWeight: "bold", color: "#1E3354" }}>
+                  Senha
+                </Form.Label>
+                <Form.Control
+                  placeholder="Senha"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridState">
                 <ContentButton>
-                  <Button onClick={register}
+                  <Button
+                    onClick={register}
                     style={{
                       width: "300px",
                       borderRadius: "30px",
                       backgroundColor: "#1E98D4",
                       color: "#fff",
                       border: "none",
-                      cursor: "pointer"
-                    }}>
+                      cursor: "pointer",
+                    }}
+                  >
                     Ir para pagamento
                   </Button>
                 </ContentButton>
@@ -204,12 +276,15 @@ function RegisterUser() {
           </Form>
         </ContentForm>
       </ContentRegister>
-      <Modal show={show} onHide={handleClose} size="lg" >
+      <Modal show={show} onHide={handleClose} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Escolha a forma de pagamento</Modal.Title>
         </Modal.Header>
         <Modal.Body style={{ height: "75vh" }}>
-          <iframe src={urlPayment} style={{ width: "100%", height: "100%" }}></iframe>
+          <iframe
+            src={urlPayment}
+            style={{ width: "100%", height: "100%" }}
+          ></iframe>
         </Modal.Body>
       </Modal>
     </MainContainer>

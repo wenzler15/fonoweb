58017273d98d1991273f5ecdbd71c348977628b4
Corvoloso <igoralves.b18@gmail.com>
2022-06-29 00:23:51 -0300

    feat - update the edit doctor form

--- src/pages/DoctorProfile/index.js
+++ src/pages/DoctorProfile/index.js
@@ -1,167 +1,194 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useAuth } from "../../hooks/auth";
 
 import api from "../../services";
 
-import TextInput from "../../components/TextInput";
 import NavBar from "../../components/navBar";
 
 import { ButtonContainer, Card, CardSelector, Container } from "./styles";
+import { useForm } from "react-hook-form";
+
+const userCards = [
+  {
+    isSelected: false,
+    number: "**** **** **** 5353",
+    expiration_date: "02/25",
+  },
+  {
+    isSelected: true,
+    number: "**** **** **** 6453",
+    expiration_date: "07/29",
+  },
+];
 
 function DoctorProfile() {
-  const { user } = useAuth();
+  const [isEditing, setIsEditing] = useState(false);
+
+  const { user, signOut } = useAuth();
   const navigate = useNavigate();
 
-  const userCards = [
-    {
-      isSelected: true,
-      number: "**** **** **** 5353",
-      expiration_date: "02/25",
-    },
-    {
-      isSelected: false,
-      number: "**** **** **** 6453",
-      expiration_date: "07/29",
-    },
-  ];
+  const { register, handleSubmit } = useForm();
+
+  const handleToggleIsEditing = () => setIsEditing((state) => !state);
+
+  const handleSubmitUpdate = useCallback((formData) => {
+    console.log(formData);
+  }, []);
+
+  const handleSignOut = useCallback(async () => {
+    await signOut();
+    navigate("/");
+  }, [navigate, signOut]);
 
   return (
     <>
       <NavBar />
 
       <Container>
-        <div>
-          <div className="titleContainer">
-            <h1>Dados Pessoais</h1>
+        <form onSubmit={handleSubmit(handleSubmitUpdate)}>
+          <div>
+            <div className="titleContainer">
+              <h1>Dados Pessoais</h1>
 
-            <button onClick={() => {}}>Editar</button>
+              <button type="button" onClick={handleToggleIsEditing}>
+                Editar
+              </button>
+            </div>
+
+            <span>
+              <input
+                disabled={isEditing}
+                title="Nome Completo"
+                placeholder="Nome completo"
+                defaultValue={user.name}
+                {...register("fullName")}
+              />
+
+              <input
+                disabled={isEditing}
+                title="Email"
+                placeholder="email@exemplo.com"
+                defaultValue={user.email}
+                {...register("email")}
+              />
+            </span>
+
+            <span>
+              <input
+                disabled={isEditing}
+                title="CPF"
+                placeholder="000.000.000-00"
+                defaultValue={user.cpf}
+                {...register("cpf")}
+              />
+              <input
+                disabled={isEditing}
+                type="file"
+                title="Anexar CRFA ou Comprovante da faculdade"
+                placeholder="PDF, PNG e JPEG"
+              />
+            </span>
           </div>
 
-          <span>
-            <TextInput
-              disabled
-              title="Nome Completo"
-              placeholder="Nome completo"
-              value={user.name}
-            />
-            <TextInput
-              disabled
-              type="email"
-              title="Email"
-              placeholder="email@exemplo.com"
-              value={user.email}
-            />
-          </span>
+          <div>
+            <div className="titleContainer">
+              <h1>
+                Endereço de atendimento <b>[opcional]</b>
+              </h1>
 
-          <span>
-            <TextInput
-              disabled
-              title="CPF"
-              placeholder="000.000.000-00"
-              value={user.cpf}
-            />
-            <TextInput
-              disabled
-              type="file"
-              title="Anexar CRFA ou Comprovante da faculdade"
-              placeholder="PDF, PNG e JPEG"
-            />
-          </span>
-        </div>
+              <button type="button" onClick={handleToggleIsEditing}>
+                Editar
+              </button>
+            </div>
 
-        <div>
-          <div className="titleContainer">
-            <h1>
-              Endereço de atendimento <b>[opcional]</b>
-            </h1>
+            <span>
+              <input
+                disabled={isEditing}
+                title="Endereço completo"
+                placeholder="Sua rua"
+                defaultValue={user.streetName}
+              />
+              <input
+                disabled={isEditing}
+                title="Número"
+                placeholder="Número da casa"
+                defaultValue={user.houseNumber}
+              />
+            </span>
 
-            <button onClick={() => {}}>Editar</button>
+            <span>
+              <input
+                disabled={isEditing}
+                title="Bairro"
+                placeholder="Seu bairro"
+                defaultValue={user.district}
+              />
+              <input
+                disabled={isEditing}
+                title="Complemento"
+                placeholder="Seu complemento"
+                defaultValue={user.complement}
+              />
+            </span>
+
+            <span>
+              <input
+                disabled={isEditing}
+                title="CEP:"
+                placeholder="Seu CEP"
+                className="lastChild"
+                defaultValue={user.cep}
+              />
+            </span>
           </div>
 
-          <span>
-            <TextInput
-              disabled
-              title="Endereço completo"
-              placeholder="Sua rua"
-              value={user?.streetName}
-            />
-            <TextInput
-              disabled
-              title="Número"
-              placeholder="Seu número"
-              value={user.houseNumber}
-            />
-          </span>
+          <div>
+            <div className="titleContainer">
+              <h1>Cobrança</h1>
 
-          <span>
-            <TextInput
-              disabled
-              title="Bairro"
-              placeholder="Seu bairro"
-              value={user?.district}
-            />
-            <TextInput
-              disabled
-              title="Complemento"
-              placeholder="Seu complemento"
-              value={user.complement}
-            />
-          </span>
+              <button type="button" onClick={handleToggleIsEditing}>
+                Adicionar nova
+              </button>
+            </div>
 
-          <span>
-            <TextInput
-              disabled
-              title="CEP:"
-              placeholder="****"
-              value={user.zipcode}
-            />
-          </span>
-        </div>
-
-        <div>
-          <div className="titleContainer">
-            <h1>Cobrança</h1>
-
-            <button onClick={() => {}}>Adicionar nova</button>
-          </div>
-
-          <div className="CardsContainer">
-            {userCards.map((card) => (
-              <>
-                <CardSelector
-                  isSelected={card.isSelected}
-                  onClick={() => console.log(card)}
-                >
-                  <strong>{card.number}</strong>
-                </CardSelector>
-
-                {card.isSelected && (
-                  <Card>
-                    <h3>Credit Card</h3>
-
+            <div className="CardsContainer">
+              {userCards.map((card) => (
+                <>
+                  <CardSelector
+                    isSelected={card.isSelected}
+                    onClick={() => console.log(card)}
+                  >
                     <strong>{card.number}</strong>
+                  </CardSelector>
 
-                    <p>
-                      Exp
-                      <br />
-                      {card.expiration_date}
-                    </p>
-                  </Card>
-                )}
-              </>
-            ))}
+                  {card.isSelected && (
+                    <Card>
+                      <h3>Credit Card</h3>
+
+                      <strong>{card.number}</strong>
+
+                      <p>
+                        Exp
+                        <br />
+                        {card.expiration_date}
+                      </p>
+                    </Card>
+                  )}
+                </>
+              ))}
+            </div>
           </div>
-        </div>
 
-        <ButtonContainer>
-          <button onClick={() => {}}>Salvar alterações</button>
+          <ButtonContainer>
+            <button type="submit">Salvar alterações</button>
 
-          <button className="logoutButton" onClick={() => navigate("/")}>
-            Sair da conta
-          </button>
-        </ButtonContainer>
+            <button className="logoutButton" onClick={handleSignOut}>
+              Sair da conta
+            </button>
+          </ButtonContainer>
+        </form>
 
         <button className="cancelButton" onClick={() => navigate("/")}>
           Cancelar minha assinatura
--- src/pages/DoctorProfile/styles.js
+++ src/pages/DoctorProfile/styles.js
@@ -4,47 +4,60 @@ export const Container = styled.div`
   width: 90%;
   margin: 0 auto;
 
-  > div {
-    display: flex;
-    flex-direction: column;
-
-    margin-top: 2rem;
-
-    .titleContainer {
+  > form {
+    > div {
       display: flex;
-      justify-content: space-between;
+      flex-direction: column;
 
-      > h1 {
-        font-family: "Poppins";
-        font-size: 1.75rem;
+      margin-top: 2rem;
 
-        > b {
-          font-size: 1rem;
-          color: #aaa;
+      .titleContainer {
+        display: flex;
+        justify-content: space-between;
+
+        > h1 {
+          font-family: "Poppins";
+          font-size: 1.75rem;
+
+          > b {
+            font-size: 1rem;
+            color: #aaa;
+          }
+        }
+
+        > button {
+          border: 0;
+          font-family: "Poppins";
+
+          padding: 1rem;
+          border-radius: 0.5rem;
+          background: #1e98d4;
+          color: #fff;
         }
       }
 
-      > button {
-        border: 0;
-        font-family: "Poppins";
+      > span {
+        display: flex;
+        width: 100%;
 
-        padding: 1rem;
-        border-radius: 0.5rem;
-        background: #1e98d4;
-        color: #fff;
-      }
-    }
+        > input {
+          border-radius: 0.25rem;
+          border: 1px solid #1e3354;
+          background: #fff;
 
-    > span {
-      display: flex;
+          font-family: "Lato";
+          padding: 0.75rem 1rem;
+          margin-top: 1rem;
+          width: 50%;
+        }
 
-      > div {
-        margin-top: 1rem;
-        width: 40%;
-      }
+        > input + input {
+          margin-left: 4rem;
+        }
 
-      > div + div {
-        margin-left: 2rem;
+        .lastChild {
+          margin-right: 4rem;
+        }
       }
     }
   }
@@ -91,11 +104,52 @@ export const Container = styled.div`
   }
 `;
 
+export const CardSelector = styled.button`
+  width: 30%;
+  display: flex;
+
+  border: 0.2px solid #aaa;
+  border-radius: 4px;
+  padding: 0.75rem;
+
+  cursor: pointer;
+  background: ${({ isSelected }) => (isSelected ? `#e5e5e5` : `transparent`)};
+
+  &::before {
+    content: "";
+    width: 1.9rem;
+    height: 1.75rem;
+    border-radius: 50%;
+    border: ${({ isSelected }) =>
+      isSelected ? `8px solid #1e98d4` : `0.2px solid #888`};
+  }
+
+  &:active {
+    margin-top: 0.2rem;
+  }
+
+  & + button {
+    margin-top: 1rem;
+  }
+
+  > strong {
+    width: 100%;
+    font-family: "Poppins";
+    font-weight: 400;
+    text-align: right;
+  }
+
+  @media (max-width: 500px) {
+    width: 100%;
+    margin-top: 1rem;
+  }
+`;
+
 export const Card = styled.div`
   margin-top: 1rem;
   margin-bottom: 1rem;
 
-  width: 30%;
+  width: 400px;
   height: 250px;
   position: relative;
   display: flex;
@@ -133,43 +187,6 @@ export const Card = styled.div`
     bottom: 0;
     right: 5%;
   }
-  @media (max-width: 500px) {
-    width: 100%;
-    margin-top: 1rem;
-  }
-`;
-
-export const CardSelector = styled.button`
-  width: 20%;
-  display: flex;
-
-  border: 0.2px solid black;
-  border-radius: 4px;
-  padding: 0.75rem;
-
-  cursor: pointer;
-  background: ${({ isSelected }) => (isSelected ? `#e5e5e5` : `transparent`)};
-
-  &::before {
-    content: "";
-    width: 1.9rem;
-    height: 1.75rem;
-    border-radius: 50%;
-    border: ${({ isSelected }) =>
-      isSelected ? `8px solid #1e98d4` : `0.2px solid black`};
-  }
-
-  &:active {
-    margin-left: 0.2rem;
-  }
-
-  > strong {
-    width: 100%;
-    font-family: "Poppins";
-    font-weight: 400;
-    text-align: right;
-  }
-
   @media (max-width: 500px) {
     width: 100%;
     margin-top: 1rem;

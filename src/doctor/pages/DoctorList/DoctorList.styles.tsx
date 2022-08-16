85e6d78db12739361aa8c65672204f30142fc7f6
Bruno Oliveira <brunohdbass@gmail.com>
2022-08-16 08:28:54 -0300

    fix: doctor list height

--- src/doctor/pages/DoctorList/DoctorList.styles.tsx
+++ src/doctor/pages/DoctorList/DoctorList.styles.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const Container = styled.div`
 	background: #e5e5e5;
 	padding-bottom: 2rem;
-	height: calc(100vh - 15px);
 `
 
 export const Content = styled.div`
@@ -13,5 +12,4 @@ export const Content = styled.div`
 	margin-left: 2rem;
 	margin-right: 2rem;
 	box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
-	height: calc(100% - 9rem);
 `

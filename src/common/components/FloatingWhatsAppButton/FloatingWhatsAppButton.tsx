fa5922b55773ab8e619942335f17f5292155a957
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2022-08-24 16:09:06 -0300

    feat: add label on whatsapp button

--- src/common/components/FloatingWhatsAppButton/FloatingWhatsAppButton.tsx
+++ src/common/components/FloatingWhatsAppButton/FloatingWhatsAppButton.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import { Container } from './styles';
+import { ButtonContent, Container, Label } from './styles';
 import ButtonBase from '@mui/material/ButtonBase';
 
 export function FloatingWhatsAppButton() {
   return (
-      <Container aria-label="whatsapp">
+    <Container>
+      <ButtonContent aria-label="whatsapp">
         {/* TODO: Add real number here */}
         <ButtonBase LinkComponent="a" target="_blank" href='https://wa.me/5516' sx={{ width: '100%', height: '100%' }}>
           <WhatsAppIcon />
         </ButtonBase>
-      </Container>
+      </ButtonContent>
+      <Label>Suporte</Label>
+    </Container>
   );
 
 }
--- src/common/components/FloatingWhatsAppButton/styles.ts
+++ src/common/components/FloatingWhatsAppButton/styles.ts
@@ -1,19 +1,35 @@
 import { styled } from '@mui/system';
 
 export const Container = styled('div')(({ theme }) => ({
+  position: 'fixed',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  bottom: theme.spacing(12),
+	right: theme.spacing(4),
+}));
+
+export const ButtonContent = styled('div')(({ theme }) => ({
   backgroundColor: '#128c7e',
   width: '50px',
   height: '50px',
   maxWidth: '50px',
   borderRadius: '50px',
-  position: 'fixed',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  // bottom: '50px',
-  // right: '30px',
   color: 'white',
-  overflow: 'hidden',
-  bottom: theme.spacing(12),
-	right: theme.spacing(4),
+  '&:hover': {
+    backgroundColor: '#075e54'
+  }
+}));
+
+export const Label = styled('label')(({ theme }) => ({
+  fontWeight: 700,
+  color: '#333',
+  textTransform: 'uppercase',
+  position: 'relative',
+  display: 'block',
+  marginTop: theme.spacing(1),
 }));

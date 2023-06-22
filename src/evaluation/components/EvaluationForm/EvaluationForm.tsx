0212dc1637a0cda6c69f9078a7de5a25223a4949
Doug <douglas.wenzler@mobitype.com>
2023-06-22 14:59:35 -0300

    Finshed

--- src/evaluation/components/EvaluationForm/EvaluationForm.tsx
+++ src/evaluation/components/EvaluationForm/EvaluationForm.tsx
@@ -1,10 +1,10 @@
 import { Editor, LoadingOverlay } from 'common/components'
 import {
-	Grid,
-	AutocompleteRenderInputParams,
-	TextField as MTextField,
-	Button,
-	Stack,
+  Grid,
+  AutocompleteRenderInputParams,
+  TextField as MTextField,
+  Button,
+  Stack,
 } from '@mui/material'
 import { Autocomplete, TextField } from 'formik-mui'
 import { Field, useFormikContext } from 'formik'
@@ -22,190 +22,250 @@ import { Specialty } from 'specialty'
 import { useSpecialties } from 'specialty/queries'
 import { EvaluationFormProps } from 'evaluation/components/EvaluationForm/EvaluationForm.types'
 import { useUniversalParam } from 'routes/hooks'
+import { useCreateTemplate } from 'template/mutations'
+import { CreateTemplateDto, CreateTemplateSchema } from 'template/schemas'
+import { TemplateForm } from 'template/components/TemplateForm/TemplateForm'
 
 export function EvaluationForm({
-	config: { canUseTemplate = true } = {},
+  config: { canUseTemplate = true } = {},
 }: EvaluationFormProps) {
-	const {
-		values: { template, patient },
-		errors,
-		touched,
-		setFieldValue,
-	} = useFormikContext<{
-		patient: UserWithPatient | null
-		template: Template | null
-		specialty: Specialty | null
-	}>()
+  const {
+    values: { template, patient },
+    errors,
+    touched,
+    setFieldValue,
+  } = useFormikContext<{
+    patient: UserWithPatient | null
+    template: Template | null
+    specialty: Specialty | null
+  }>()
 
-	const patientId = useUniversalParam('patient')
-	const templateId = useUniversalParam('template')
+  const [showModal, setShowModal] = useState(false);
 
-	const [editorState, setEditorState] = useState<EditorState>(() =>
-		EditorState.createEmpty(),
-	)
+  const patientId = useUniversalParam('patient')
+  const templateId = useUniversalParam('template')
 
-	const specialties = useSpecialties({
-		page: 1,
-		size: 9999,
-	})
+  const [editorState, setEditorState] = useState<EditorState>(() =>
+    EditorState.createEmpty(),
+  )
 
-	const patients = usePatients({
-		page: 1,
-		size: 9999,
-	})
+  const specialties = useSpecialties({
+    page: 1,
+    size: 9999,
+  })
 
-	const templates = useTemplates({
-		page: 1,
-		size: 9999,
-	})
+  const patients = usePatients({
+    page: 1,
+    size: 9999,
+  })
 
-	useEffect(() => {
-		if (patientId && !patient) {
-			setFieldValue(
-				'patient',
-				patients.data?.result.find(p => p.patientData.id === patientId),
-			)
-		}
-	}, [patientId, patient, patients.data?.result, setFieldValue])
+  const templates = useTemplates({
+    page: 1,
+    size: 9999,
+  })
 
-	const handleTemplateChange = async ({ html }: Template) => {
-		try {
-			const blocksFromHtml = htmlToDraft(html)
-			const { contentBlocks, entityMap } = blocksFromHtml
-			const contentState = ContentState.createFromBlockArray(
-				contentBlocks,
-				entityMap,
-			)
-			const newEditorState = EditorState.createWithContent(contentState)
-			setEditorState(newEditorState)
-			setFieldValue('text', html)
-		} catch (error) {
-			console.error(error)
-			return Swal.fire({
-				title: 'Ops!',
-				text: 'Não foi possível utilizar o PDF para edição. Tente novamente.',
-				icon: 'error',
-				confirmButtonText: 'Voltar',
-			})
-		}
+  useEffect(() => {
+    if (patientId && !patient) {
+      setFieldValue(
+        'patient',
+        patients.data?.result.find(p => p.patientData.id === patientId),
+      )
+    }
+  }, [patientId, patient, patients.data?.result, setFieldValue])
 
-		return undefined
-	}
+  const handleTemplateChange = async ({ html }: Template) => {
+    try {
+      const blocksFromHtml = htmlToDraft(html)
+      const { contentBlocks, entityMap } = blocksFromHtml
+      const contentState = ContentState.createFromBlockArray(
+        contentBlocks,
+        entityMap,
+      )
+      const newEditorState = EditorState.createWithContent(contentState)
+      setEditorState(newEditorState)
+      setFieldValue('text', html)
+    } catch (error) {
+      console.error(error)
+      return Swal.fire({
+        title: 'Ops!',
+        text: 'Não foi possível utilizar o PDF para edição. Tente novamente.',
+        icon: 'error',
+        confirmButtonText: 'Voltar',
+      })
+    }
 
-	const modalTemplates = useVisible()
+    return undefined
+  }
 
-	useEffect(() => {
-		if (template) {
-			handleTemplateChange(template)
-		}
+  const modalTemplates = useVisible()
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [template])
+  useEffect(() => {
+    if (template) {
+      handleTemplateChange(template)
+    }
 
-	useTemplateDetail(templateId as string, {
-		enabled: !!templateId,
-		onSuccess: ({ result }) => {
-			handleTemplateChange(result)
-		},
-	})
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [template])
 
-	return (
-		<>
-			<Grid container spacing={2}>
-				<Grid
-					item
-					xs={12}
-					container
-					spacing={2}
-					direction="row"
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Grid item xs={12}>
-						<Stack direction="row" spacing={2}>
-							<Field
-								fullWidth
-								component={TextField}
-								name="title"
-								label="Título"
-							/>
-							{canUseTemplate && (
-								<Button
-									sx={{ width: '20%' }}
-									onClick={() => modalTemplates.show()}
-									variant="contained"
-									size="large"
-									color="secondary"
-								>
-									Selecionar Modelo
-								</Button>
-							)}
-						</Stack>
-					</Grid>
-					<Grid item xs={6}>
-						<Field
-							fullWidth
-							name="specialty"
-							component={Autocomplete}
-							options={specialties.data?.result ?? []}
-							getOptionLabel={(option: Specialty) => option.name}
-							renderInput={(params: AutocompleteRenderInputParams) => (
-								<MTextField
-									{...params}
-									name="specialty-search"
-									error={touched.patient && !!errors.patient}
-									helperText={errors.specialty}
-									label="Especialidade"
-									variant="outlined"
-								/>
-							)}
-						/>
-					</Grid>
-					<Grid item xs={6}>
-						{patientId && (
-							<MTextField
-								fullWidth
-								disabled
-								label="Paciente"
-								value={
-									patients.data?.result.find(
-										p => p.patientData.id === patientId,
-									)?.name
-								}
-							/>
-						)}
-						{!patientId && (
-							<Field
-								fullWidth
-								name="patient"
-								component={Autocomplete}
-								options={patients.data?.result ?? []}
-								getOptionLabel={(option: UserWithPatient) => option.name}
-								renderInput={(params: AutocompleteRenderInputParams) => (
-									<MTextField
-										{...params}
-										name="patient-search"
-										error={touched.patient && !!errors.patient}
-										helperText={errors.patient}
-										label="Paciente"
-										variant="outlined"
-									/>
-								)}
-							/>
-						)}
-					</Grid>
-				</Grid>
-				<Grid item xs={12}>
-					<Editor name="text" editorState={editorState} />
-				</Grid>
-			</Grid>
-			<LoadingOverlay show={templates.isLoading || patients.isLoading} />
-			<TemplateSelector
-				type={TemplateType.EVALUATION}
-				visible={modalTemplates.visible}
-				onClose={modalTemplates.hide}
-				onSelect={handleTemplateChange}
-			/>
-		</>
-	)
+  const createTemplate = useCreateTemplate({
+    onSuccess: () => {
+      setShowModal(false)
+      Swal.fire({
+        title: 'Success',
+        text: 'Modelo criado com sucesso',
+        icon: 'success',
+      })
+    },
+  })
+
+  const handleFormSubmit = async ({
+    specialty,
+    ...values
+  }: CreateTemplateDto) => {
+    await createTemplate
+      .mutateAsync({
+        ...values,
+        specialtyId: specialty.id,
+      })
+      .catch(console.error)
+  }
+
+  useTemplateDetail(templateId as string, {
+    enabled: !!templateId,
+    onSuccess: ({ result }) => {
+      handleTemplateChange(result)
+    },
+  })
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          container
+          spacing={2}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <Stack direction="row" spacing={2}>
+              <Field
+                sx={{ width: '60%' }}
+                component={TextField}
+                name="title"
+                label="Título"
+              />
+              {canUseTemplate && (
+                <Button
+                  sx={{ width: '20%' }}
+                  onClick={() => modalTemplates.show()}
+                  variant="contained"
+                  size="large"
+                  color="secondary"
+                >
+                  Selecionar Modelo
+                </Button>
+              )}
+              <Button
+                onClick={() => setShowModal(true)}
+                variant="contained"
+                size="large"
+                color="secondary"
+              >
+                Criar Modelo
+              </Button>
+            </Stack>
+          </Grid>
+          {showModal && (
+            <div style={{ background: '#fff', position: 'absolute', width: '90%', padding: 20, border: '1px solid #000', borderRadius: 10, zIndex: 1, marginTop: 50 }}>
+              <div onClick={() => setShowModal(false)} style={{ width: '100%', display: 'flex', justifyContent: 'flex-end', marginBottom: 20, cursor: 'pointer' }}>
+                <div style={{ border: '1px solid #FFF', borderRadius: 20, width: 30, height: 30, background: '#ff4040' }}>
+                  <p style={{ fontSize: 16, marginLeft: 9, marginTop: 2, color: '#fff' }}>X</p>
+                </div>
+              </div>
+              <TemplateForm<CreateTemplateDto>
+                schema={CreateTemplateSchema}
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                onSubmit={handleFormSubmit}
+                initialValues={{
+                  title: '',
+                  html: '',
+                  // @ts-expect-error null
+                  type: null,
+                  // @ts-expect-error null
+                  specialty: specialties.data?.result,
+                }}
+              />
+            </div>
+          )}
+          <Grid item xs={6} style={{ opacity: showModal ? 0.5 : 1 }}>
+            <Field
+              fullWidth
+              name="specialty"
+              component={Autocomplete}
+              options={specialties.data?.result ?? []}
+              getOptionLabel={(option: Specialty) => option.name}
+              renderInput={(params: AutocompleteRenderInputParams) => (
+                <MTextField
+                  {...params}
+                  name="specialty-search"
+                  error={touched.patient && !!errors.patient}
+                  helperText={errors.specialty}
+                  label="Especialidade"
+                  variant="outlined"
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={6} style={{ opacity: showModal ? 0.5 : 1 }}>
+            {patientId && (
+              <MTextField
+                fullWidth
+                disabled
+                label="Paciente"
+                value={
+                  patients.data?.result.find(
+                    p => p.patientData.id === patientId,
+                  )?.name
+                }
+              />
+            )}
+            {!patientId && (
+              <Field
+                fullWidth
+                name="patient"
+                component={Autocomplete}
+                options={patients.data?.result ?? []}
+                getOptionLabel={(option: UserWithPatient) => option.name}
+                renderInput={(params: AutocompleteRenderInputParams) => (
+                  <MTextField
+                    {...params}
+                    name="patient-search"
+                    error={touched.patient && !!errors.patient}
+                    helperText={errors.patient}
+                    label="Paciente"
+                    variant="outlined"
+                  />
+                )}
+              />
+            )}
+          </Grid>
+        </Grid>
+        {!showModal && (
+          <Grid item xs={12}>
+            <Editor name="text" editorState={editorState} />
+          </Grid>
+        )}
+      </Grid>
+      <LoadingOverlay show={templates.isLoading || patients.isLoading} />
+      <TemplateSelector
+        type={TemplateType.EVALUATION}
+        visible={modalTemplates.visible}
+        onClose={modalTemplates.hide}
+        onSelect={handleTemplateChange}
+      />
+    </>
+  )
 }
--- src/evaluation/pages/EvaluationCreate/EvaluationCreate.tsx
+++ src/evaluation/pages/EvaluationCreate/EvaluationCreate.tsx
@@ -12,88 +12,88 @@ import Swal from 'sweetalert2'
 import { InferType } from 'yup'
 
 export function EvaluationCreate() {
-	const navigate = useNavigate()
-	const createEvaluation = useCreateEvaluation({
-		onSuccess: () => {
-			navigate('/patients')
-			Swal.fire({
-				title: 'Success',
-				text: 'Avaliação criada com sucesso',
-				icon: 'success',
-			})
-		},
-	})
+  const navigate = useNavigate()
+  const createEvaluation = useCreateEvaluation({
+    onSuccess: () => {
+      navigate('/patients')
+      Swal.fire({
+        title: 'Success',
+        text: 'Avaliação criada com sucesso',
+        icon: 'success',
+      })
+    },
+  })
 
-	const handleFormSubmit = async ({
-		patient,
-		specialty,
-		...values
-	}: InferType<typeof CreateEvaluationSchema>) => {
-		await createEvaluation
-			.mutateAsync({
-				...omit(['template'], values),
-				patientId: patient!.patientData.id,
-				specialtyId: specialty!.id,
-			})
-			.catch(console.error)
-	}
+  const handleFormSubmit = async ({
+    patient,
+    specialty,
+    ...values
+  }: InferType<typeof CreateEvaluationSchema>) => {
+    await createEvaluation
+      .mutateAsync({
+        ...omit(['template'], values),
+        patientId: patient!.patientData.id,
+        specialtyId: specialty!.id,
+      })
+      .catch(console.error)
+  }
 
-	return (
-		<>
-			<Formik<InferType<typeof CreateEvaluationSchema>>
-				validationSchema={CreateEvaluationSchema}
-				initialValues={{
-					text: '',
-					title: '',
-					exercises: [],
-					appointmentDate: new Date(),
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					patient: null as any,
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					specialty: null as any,
-				}}
-				onSubmit={handleFormSubmit}
-			>
-				{({ handleSubmit }) => (
-					<>
-						
-						<Box sx={{ p: t => t.spacing(4) }}>
-							<Box
-								sx={{
-									display: 'flex',
-									alignItems: 'center',
-									justifyContent: 'space-between',
-								}}
-							>
-								<Typography
-									variant="h4"
-									component="h1"
-									color="secondary"
-									sx={{ mb: t => t.spacing(2) }}
-								>
-									Nova avaliação
-									<Back />
-								</Typography>
-								<LoadingButton
-									onClick={() => handleSubmit()}
-									variant="contained"
-									color="secondary"
-									size="large"
-									loading={createEvaluation.isLoading}
-								>
-									SALVAR
-								</LoadingButton>
-							</Box>
-							<Card>
-								<CardContent sx={{ p: t => t.spacing(4) }}>
-									<EvaluationForm />
-								</CardContent>
-							</Card>
-						</Box>
-					</>
-				)}
-			</Formik>
-			
-		</>
-	)
+  return (
+    <>
+      <Formik<InferType<typeof CreateEvaluationSchema>>
+        validationSchema={CreateEvaluationSchema}
+        initialValues={{
+          text: '',
+          title: '',
+          exercises: [],
+          appointmentDate: new Date(),
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          patient: null as any,
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          specialty: null as any,
+        }}
+        onSubmit={handleFormSubmit}
+      >
+        {({ handleSubmit }) => (
+          <>
+
+            <Box sx={{ p: t => t.spacing(4) }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Typography
+                  variant="h4"
+                  component="h1"
+                  color="secondary"
+                  sx={{ mb: t => t.spacing(2) }}
+                >
+                  Nova avaliação
+                  <Back />
+                </Typography>
+                <LoadingButton
+                  onClick={() => handleSubmit()}
+                  variant="contained"
+                  color="secondary"
+                  size="large"
+                  loading={createEvaluation.isLoading}
+                >
+                  SALVAR
+                </LoadingButton>
+              </Box>
+              <Card>
+                <CardContent sx={{ p: t => t.spacing(4) }}>
+                  <EvaluationForm />
+                </CardContent>
+              </Card>
+            </Box>
+          </>
+        )}
+      </Formik>
+
+    </>
+  )
 }
